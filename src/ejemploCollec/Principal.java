1131651b7f737598509b40ffd688d72515947a6c
Candi <Alcantarilla.cacan24@triana.salesianos.edu>
2024-02-14 10:10:18 +0100

    ejemploAddAll

---
+++ src/ejemploCollec/Principal.java
@@ -0,0 +1,25 @@
+package ejemploCollec;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		/*Aquí vamos a mostrar como es la sintaxis de como 
+		 * se crearía una colección*/
+		
+		/*Lo primero que pondremos será el tipo de colección que usaremos*/
+		/*Luego entre mayor que y menor que pondremos la clase de objetos 
+		 * que vamos a almacenar*/
+		/*Pondremos el nombre que deseamos ponerle*/
+		/*Por último pondremos el típico constructor con el mismo nmbre de la clase 
+		 * entre mayor y menos que*/
+		
+			List<String> lista = new ArrayList<String>();
+
+	}
+
+}
+  
